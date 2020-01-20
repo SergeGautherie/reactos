9d0ed970f3e21800bece5be3aa5f5d5102ed820a
Serge Gautherie <reactos-git_serge_171003@gautherie.fr>
2020-01-20 21:59:32 +0100

    Revert "[KMTESTS:OB] ObTypes: Use 'versionhelpers.h'"
    This reverts commit 3cbe64a4857d5087e195532374b9c72db9a2e014.

--- modules/rostests/kmtests/ntos_ob/ObTypes.c
+++ modules/rostests/kmtests/ntos_ob/ObTypes.c
@@ -6,7 +6,6 @@
  */
 
 #include <kmt_test.h>
-#include <versionhelpers.h>
 
 #define NDEBUG
 #include <debug.h>
@@ -158,7 +157,7 @@ TestObjectTypes(VOID)
 {
     ULONG Index;
 
-    if (skip(!IsWindows7OrGreater(), "TODO on NT6.1+: GetObjectType() fails\n"))
+    if (skip(g_NtVersion <= 0x0600, "TODO on NT6.1+: GetObjectType() fails\n"))
     {
         // NB: ObOpenObjectByName() returns 0xc000000d.
 
@@ -232,8 +231,8 @@ TestObjectTypes(VOID)
     CheckObjectType(Section, MmSectionObjectType,               OBT_PAGED_POOL,                             0x100,  0x020005, 0x020002, 0x020008, 0x0f001f, 0x1f001f);
     CheckObjectType(Key, CmpKeyObjectType,                      OBT_CUSTOM_SECURITY_PROC | OBT_SECURITY_REQUIRED | OBT_PAGED_POOL,
                                                                                                             0x030,  0x020019, 0x020006, 0x020019, 0x0f003f, 0x1f003f);
-    CheckObjectType(Port, LpcPortObjectType,                    OBT_PAGED_POOL, !IsWindowsVistaOrGreater() ? 0x7b2 : 0xfb2,  0x020001, 0x010001, 0x000000, 0x1f0001, 0x1f0001);
-    CheckObjectType(WaitablePort, LpcWaitablePortObjectType,    OBT_NO_DEFAULT, !IsWindowsVistaOrGreater() ? 0x7b2 : 0xfb2,  0x020001, 0x010001, 0x000000, 0x1f0001, 0x1f0001);
+    CheckObjectType(Port, LpcPortObjectType,                    OBT_PAGED_POOL, g_NtVersion <= 0x0501 ? 0x7b2 : 0xfb2,  0x020001, 0x010001, 0x000000, 0x1f0001, 0x1f0001);
+    CheckObjectType(WaitablePort, LpcWaitablePortObjectType,    OBT_NO_DEFAULT, g_NtVersion <= 0x0501 ? 0x7b2 : 0xfb2,  0x020001, 0x010001, 0x000000, 0x1f0001, 0x1f0001);
     CheckObjectType(Adapter, IoAdapterObjectType,               0,                                          0x100,  0x120089, 0x120116, 0x1200a0, 0x1f01ff, 0x1f01ff);
     CheckObjectType(Controller, IoControllerObjectType,         0,                                          0x100,  0x120089, 0x120116, 0x1200a0, 0x1f01ff, 0x1f01ff);
     CheckObjectType(Device, IoDeviceObjectType,                 OBT_CUSTOM_SECURITY_PROC | OBT_CASE_INSENSITIVE,
@@ -243,7 +242,7 @@ TestObjectTypes(VOID)
     CheckObjectType(File, *IoFileObjectType,                     OBT_NO_DEFAULT | OBT_CUSTOM_SECURITY_PROC | OBT_CASE_INSENSITIVE | OBT_MAINTAIN_HANDLE_COUNT,
                                                                                                             0x130,  0x120089, 0x120116, 0x1200a0, 0x1f01ff, 0x1f01ff);
     CheckObjectType(WmiGuid, WmipGuidObjectType,                OBT_NO_DEFAULT | OBT_CUSTOM_SECURITY_PROC | OBT_SECURITY_REQUIRED,
-                                                                                                            0x100,  !IsWindowsVistaOrGreater() ? 0x020001 :  0x000001, !IsWindowsVistaOrGreater() ? 0x020002 :  0x000002, !IsWindowsVistaOrGreater() ? 0x020010 :  0x000010, 0x120fff, 0x1f0fff);
+                                                                                                            0x100,  g_NtVersion <= 0x0501 ? 0x020001 :  0x000001, g_NtVersion <= 0x0501 ? 0x020002 :  0x000002, g_NtVersion <= 0x0501 ? 0x020010 :  0x000010, 0x120fff, 0x1f0fff);
     CheckObjectType(FilterConnectionPort, NULL,                 OBT_NO_DEFAULT | OBT_SECURITY_REQUIRED,     0x100,  0x020001, 0x010001, 0x000000, 0x1f0001, 0x1f0001);
     CheckObjectType(FilterCommunicationPort, NULL,              OBT_NO_DEFAULT,                             0x100,  0x020001, 0x010001, 0x000000, 0x1f0001, 0x1f0001);
 
