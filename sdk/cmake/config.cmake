3f6286f00d6a9fea7aa55020c00f9628e7d039ca
Serge Gautherie <reactos-git_serge_171003@gautherie.fr>
2020-05-19 21:59:12 +0200

    config.cmake: Tweak some settings
    * OARCH "pentium2" + TUNE "pentium3"
    * MSVC: KDBG ... + _WINKD_ FALSE
    * BUILD_MP FALSE
    
    SelfOnly

--- sdk/cmake/config.cmake
+++ sdk/cmake/config.cmake
@@ -19,7 +19,7 @@ elseif(ARCH STREQUAL "arm64")
 endif()
 
 if(ARCH STREQUAL "i386")
-    set(OARCH "pentium" CACHE STRING
+    set(OARCH "pentium2" CACHE STRING
     "Generate instructions for this CPU type. Specify one of:
      pentium, pentiumpro")
 elseif(ARCH STREQUAL "amd64")
@@ -39,7 +39,7 @@ elseif(ARCH STREQUAL "arm64")
 endif()
 
 if(ARCH STREQUAL "i386" OR ARCH STREQUAL "amd64")
-    set(TUNE "generic" CACHE STRING
+    set(TUNE "pentium3" CACHE STRING
     "Which CPU ReactOS should be optimized for.")
 elseif(ARCH STREQUAL "arm")
     set(TUNE "generic-armv7-a" CACHE STRING
@@ -95,14 +95,12 @@ else()
 endif()
 
 if(MSVC)
-    set(KDBG FALSE CACHE BOOL
-"Whether to compile in the integrated kernel debugger.")
     if(CMAKE_BUILD_TYPE STREQUAL "Release")
-        set(_WINKD_ FALSE CACHE BOOL "Whether to compile with the KD protocol.")
+        set(KDBG FALSE CACHE BOOL "Whether to compile in the integrated kernel debugger.")
     else()
-        set(_WINKD_ TRUE CACHE BOOL "Whether to compile with the KD protocol.")
+        set(KDBG TRUE CACHE BOOL "Whether to compile in the integrated kernel debugger.")
     endif()
-
+    set(_WINKD_ FALSE CACHE BOOL "Whether to compile with the KD protocol.")
 else()
     if(CMAKE_BUILD_TYPE STREQUAL "Release")
         set(KDBG FALSE CACHE BOOL "Whether to compile in the integrated kernel debugger.")
@@ -112,7 +110,7 @@ else()
     set(_WINKD_ FALSE CACHE BOOL "Whether to compile with the KD protocol.")
 endif()
 
-option(BUILD_MP "Whether to build the multiprocessor versions of NTOSKRNL and HAL." ON)
+option(BUILD_MP "Whether to build the multiprocessor versions of NTOSKRNL and HAL." OFF)
 
 cmake_dependent_option(ISAPNP_ENABLE "Whether to enable the ISA PnP support." ON
                        "ARCH STREQUAL i386 AND NOT SARCH STREQUAL xbox" OFF)
