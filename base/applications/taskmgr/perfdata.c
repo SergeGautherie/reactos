c95962efaeae9c4ad2f0d7100dcfa0645774467f
Serge Gautherie <reactos-git_serge_171003@gautherie.fr>
2020-06-10 10:09:14 +0200

    [TASKMGR] Properly handle start failures

--- base/applications/taskmgr/perfdata.c
+++ base/applications/taskmgr/perfdata.c
@@ -30,6 +30,9 @@
 #include <ndk/psfuncs.h>
 #include <ndk/exfuncs.h>
 
+#define NDEBUG
+#include <reactos/debug.h>
+
 CRITICAL_SECTION                           PerfDataCriticalSection;
 PPERFDATA                                  pPerfData = NULL;
 ULONG                                      ProcessCount = 0;
@@ -62,19 +65,28 @@ BOOL PerfDataInitialize(void)
     SID_IDENTIFIER_AUTHORITY NtSidAuthority = {SECURITY_NT_AUTHORITY};
     NTSTATUS    status;
 
-    InitializeCriticalSection(&PerfDataCriticalSection);
-
     /*
      * Get number of processors in the system
      */
     status = NtQuerySystemInformation(SystemBasicInformation, &SystemBasicInfo, sizeof(SystemBasicInfo), NULL);
     if (!NT_SUCCESS(status))
+    {
+        DPRINT1("NtQuerySystemInformation(SystemBasicInformation) failed! (%#lx)\n", status);
         return FALSE;
+    }
 
     /*
      * Create the SYSTEM Sid
      */
-    AllocateAndInitializeSid(&NtSidAuthority, 1, SECURITY_LOCAL_SYSTEM_RID, 0, 0, 0, 0, 0, 0, 0, &SystemUserSid);
+    if (!AllocateAndInitializeSid(&NtSidAuthority,
+                                  1, SECURITY_LOCAL_SYSTEM_RID, 0, 0, 0, 0, 0, 0, 0,
+                                  &SystemUserSid))
+    {
+        DPRINT1("AllocateAndInitializeSid() failed. (%lu)\n", GetLastError());
+    }
+
+    InitializeCriticalSection(&PerfDataCriticalSection);
+
     return TRUE;
 }
 
@@ -91,7 +103,6 @@ void PerfDataUninitialize(void)
     if (SystemUserSid != NULL)
     {
         FreeSid(SystemUserSid);
-        SystemUserSid = NULL;
     }
 
     /* Free user names cache list */
