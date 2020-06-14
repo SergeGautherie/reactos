0b799db1a3695306c192eac452caa89819faaae5
Serge Gautherie <reactos-git_serge_171003@gautherie.fr>
2020-06-14 13:49:22 +0200

    EARLY_DEBUG centralize (APIC) Future?

--- hal/halx86/apic/halinit.c
+++ hal/halx86/apic/halinit.c
@@ -14,6 +14,31 @@
 #define NDEBUG
 #include <debug.h>
 
+// Untested here. #define EARLY_DEBUG
+
+#if defined(EARLY_DEBUG)
+
+ULONG (*FrLdrDbgPrint)(_In_ _Printf_format_string_ PCSTR Format, ...);
+
+#undef DbgPrint
+#define DbgPrint FrLdrDbgPrint
+
+#else
+
+#if 0
+
+#undef DbgPrint
+
+#if defined(_MSC_VER)
+#define DbgPrint __noop
+#else
+#define DbgPrint
+#endif // _MSC_VER
+
+#endif // 0
+
+#endif // EARLY_DEBUG
+
 VOID
 NTAPI
 ApicInitializeLocalApic(ULONG Cpu);
@@ -26,24 +51,49 @@ HalpInitProcessor(
     IN ULONG ProcessorNumber,
     IN PLOADER_PARAMETER_BLOCK LoaderBlock)
 {
+#if defined(EARLY_DEBUG)
+    if (LoaderBlock)
+    {
+        /* Define your own function or use the trick with FreeLoader */
+        FrLdrDbgPrint = ((PLOADER_PARAMETER_BLOCK)LoaderBlock)->u.I386.CommonDataArea;
+    }
+#if 0 // Future
+    else
+    {
+        /* Define your own function or use the trick with FreeLoader */
+        FrLdrDbgPrint = DPRINT1;
+    }
+#endif
+#endif
+
 #ifdef CONFIG_SMP
     if (ProcessorNumber == 0)
     {
 #endif
+        DPRINT1("1a\n");
         HalpParseApicTables(LoaderBlock);
+        DPRINT1("1b\n");
 // Local debug only.
+        DPRINT1("Aa\n");
         HalpPrintApicTables();
+        DPRINT1("Ab\n");
 #ifdef CONFIG_SMP
     }
 
+    DPRINT1("2a\n");
     HalpSetupProcessorsTable(ProcessorNumber);
+    DPRINT1("2a\n");
 #endif
 
     /* Initialize the local APIC for this cpu */
+    DPRINT1("3a\n");
     ApicInitializeLocalApic(ProcessorNumber);
+    DPRINT1("3b\n");
 
     /* Initialize profiling data (but don't start it) */
+    DPRINT1("4a\n");
     HalInitializeProfiling();
+    DPRINT1("4b\n");
 
     /* Initialize the timer */
     //ApicInitializeTimer(ProcessorNumber);
@@ -56,22 +106,28 @@ HalpInitPhase0(IN PLOADER_PARAMETER_BLOCK LoaderBlock)
             (HalpBuildType & PRCB_BUILD_UNIPROCESSOR) ? "UP" : "SMP",
             (HalpBuildType & PRCB_BUILD_DEBUG) ? "DBG" : "REL");
 
+    DPRINT1("5a\n");
     HalpPrintApicTables();
+    DPRINT1("5b\n");
 
     /* Enable clock interrupt handler */
+    DPRINT1("6a\n");
     HalpEnableInterruptHandler(IDT_INTERNAL,
                                0,
                                APIC_CLOCK_VECTOR,
                                CLOCK2_LEVEL,
                                HalpClockInterrupt,
                                Latched);
+    DPRINT1("6b\n");
 }
 
 VOID
 HalpInitPhase1(VOID)
 {
     /* Initialize DMA. NT does this in Phase 0 */
+    DPRINT1("7a\n");
     HalpInitDma();
+    DPRINT1("7b\n");
 }
 
 /* EOF */
