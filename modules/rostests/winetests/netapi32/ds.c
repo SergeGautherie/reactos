13a081879c022d91df82788ecfd0cc4588d9d0e8
Serge Gautherie <reactos-git_serge_171003@gautherie.fr>
2020-09-09 03:46:56 +0200

    Merge ROS netapi32_apitest into WINE netapi32_winetest...

--- modules/rostests/winetests/netapi32/ds.c
+++ modules/rostests/winetests/netapi32/ds.c
@@ -34,18 +34,14 @@ static void test_params(void)
     DWORD ret;
     PDSROLE_PRIMARY_DOMAIN_INFO_BASIC dpdi;
 
-    SetLastError(0xdeadbeef);
     ret = pDsRoleGetPrimaryDomainInformation(NULL, DsRolePrimaryDomainInfoBasic, NULL);
     ok( ret == ERROR_INVALID_PARAMETER, "Expected error ERROR_INVALID_PARAMETER, got (%d)\n", ret);
 
-    SetLastError(0xdeadbeef);
     ret = pDsRoleGetPrimaryDomainInformation(NULL, 0, NULL);
     ok( ret == ERROR_INVALID_PARAMETER, "Expected error ERROR_INVALID_PARAMETER, got (%d)\n", ret);
-    SetLastError(0xdeadbeef);
     ret = pDsRoleGetPrimaryDomainInformation(NULL, 4, NULL);
     ok( ret == ERROR_INVALID_PARAMETER, "Expected error ERROR_INVALID_PARAMETER, got (%d)\n", ret);
 
-    SetLastError(0xdeadbeef);
     ret = pDsRoleGetPrimaryDomainInformation(NULL, 4, (PBYTE *)&dpdi);
     ok( ret == ERROR_INVALID_PARAMETER, "Expected error ERROR_INVALID_PARAMETER, got (%d)\n", ret);
 }
@@ -57,17 +53,14 @@ static void test_get(void)
     PDSROLE_UPGRADE_STATUS_INFO dusi;
     PDSROLE_OPERATION_STATE_INFO dosi;
 
-    SetLastError(0xdeadbeef);
     ret = pDsRoleGetPrimaryDomainInformation(NULL, DsRolePrimaryDomainInfoBasic, (PBYTE *)&dpdi);
     ok( ret == ERROR_SUCCESS, "Expected ERROR_SUCCESS, got (%d)\n", ret);
     pDsRoleFreeMemory(dpdi);
 
-    SetLastError(0xdeadbeef);
     ret = pDsRoleGetPrimaryDomainInformation(NULL, DsRoleUpgradeStatus, (PBYTE *)&dusi);
     todo_wine { ok( ret == ERROR_SUCCESS, "Expected ERROR_SUCCESS, got (%d)\n", ret); }
     pDsRoleFreeMemory(dusi);
    
-    SetLastError(0xdeadbeef);
     ret = pDsRoleGetPrimaryDomainInformation(NULL, DsRoleOperationState, (PBYTE *)&dosi);
     todo_wine { ok( ret == ERROR_SUCCESS, "Expected ERROR_SUCCESS, got (%d)\n", ret); }
     pDsRoleFreeMemory(dosi);
