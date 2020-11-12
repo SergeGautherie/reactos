7a7a67a82a7d35301fb4f60faf84f68aa307a20d
Serge Gautherie <reactos-git_serge_171003@gautherie.fr>
2020-11-12 01:22:46 +0100

    libwine/config.c: Test arm defines.

--- sdk/lib/3rdparty/libwine/config.c
+++ sdk/lib/3rdparty/libwine/config.c
@@ -18,6 +18,92 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
  */
 
+/**/
+
+// Win32
+
+#if 1 && defined(WIN32)
+#error WIN32 is defined (libwine/config)
+#endif
+
+#if 0 && defined(_WIN32)
+#error _WIN32 is defined (libwine/config)
+#endif
+
+// Win64
+
+#if 1 && defined(WIN64)
+#error WIN64 is defined (libwine/config)
+#endif
+
+#if 1 && defined(_WIN64)
+#error _WIN64 is defined (libwine/config)
+#endif
+
+// i386
+
+#if 1 && defined(i386)
+#error i386 is defined (libwine/config)
+#endif
+
+#if 1 && defined(__i386__)
+#error __i386__ is defined (libwine/config)
+#endif
+
+#if 1 && defined(_X86_)
+#error _X86_ is defined (libwine/config)
+#endif
+
+#if 1 && defined(_M_IX86)
+#error _M_IX86 is defined (libwine/config)
+#endif
+
+// amd64
+
+#if 1 && defined(__x86_64__)
+#error __x86_64__ is defined (libwine/config)
+#endif
+
+#if 1 && defined(_AMD64_)
+#error _AMD64_ is defined (libwine/config)
+#endif
+
+#if 1 && defined(_M_AMD64)
+#error _M_AMD64 is defined (libwine/config)
+#endif
+
+// arm
+
+#if 1 && defined(__arm__)
+#error __arm__ is defined (libwine/config)
+#endif
+
+#if 1 && defined(_ARM_)
+#error _ARM_ is defined (libwine/config)
+#endif
+
+#if 0 && defined(_M_ARM)
+#error _M_ARM is defined (libwine/config)
+#endif
+
+// arm64
+
+#if 1 && defined(__arm64__)
+#error __arm64__ is defined (libwine/config)
+#endif
+
+#if 1 && defined(_ARM64_)
+#error _ARM64_ is defined (libwine/config)
+#endif
+
+#if 1 && defined(_M_ARM64)
+#error _M_ARM64 is defined (libwine/config)
+#endif
+
+#error no more are defined (libwine/config)
+
+/**/
+
 #include "wine/library.h"
 
 /* retrieve the wine data dir */
