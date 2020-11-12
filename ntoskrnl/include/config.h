3d7e3a4f9114191879d5159e9a9d9f60c075bdea
Serge Gautherie <reactos-git_serge_171003@gautherie.fr>
2020-11-12 01:24:06 +0100

    Enable ENABLE_SPECIAL_POOL_DEFAULT_AND_WIN32K

--- ntoskrnl/include/config.h
+++ ntoskrnl/include/config.h
@@ -4,7 +4,7 @@
 #define ENABLE_DRIVER_VERIFIER_OBREFERENCEOBJECTBYHANDLE
 
 // Enable Special Pool for everything, but modules which are not win32k.sys. (CORE-10380)
-// #define ENABLE_SPECIAL_POOL_DEFAULT_AND_WIN32K
+#define ENABLE_SPECIAL_POOL_DEFAULT_AND_WIN32K
 
 // Enable global page support.
 // #define _GLOBAL_PAGES_ARE_AWESOME_
