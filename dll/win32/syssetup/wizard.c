82f937f4e87c2e92afc2de2a5cd2ab1505fbdfcb
Serge Gautherie <reactos-git_serge_171003@gautherie.fr>
2025-05-28 16:54:04 +0200

    wizard Locale

--- dll/win32/syssetup/wizard.c
+++ dll/win32/syssetup/wizard.c
@@ -1466,7 +1466,7 @@ WriteUserLocale(VOID)
                             KEY_WRITE, NULL, &hKey, NULL) == ERROR_SUCCESS)
         {
 // Stage 2 interactive: que 0409 !
-            DPRINT1("CORE-15848, S2, to Reg: Locale='%S'\n", Locale);
+            DPRINT1("CORE-15848, S2, to Reg: Locale = '%S'\n", Locale);
             ASSERT(wcslen(Locale) == 8);
             RegSetValueExW(hKey, L"Locale", 0, REG_SZ, (LPBYTE)Locale, (wcslen(Locale) + 1) * sizeof(WCHAR));
             RegCloseKey(hKey);
